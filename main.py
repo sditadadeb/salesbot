d00758544cf315dd90c429e78c0d82b819577e6e
sditadadeb <81593825+sditadadeb@users.noreply.github.com>
2025-07-07 21:47:26 +0000

    cambios

--- main.py
+++ main.py
@@ -1,18 +1,17 @@
 from fastapi import FastAPI, Request
-
 from fastapi.middleware.cors import CORSMiddleware
+import httpx
 
+app = FastAPI()
+
+# Middleware CORS — esto ahora sí funciona
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],  # En producción conviene restringir esto
+    allow_origins=["*"],  # En producción restringí esto
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-import httpx
-
-app = FastAPI()
 
 # Pegá tu webhook de Google Chat acá (el que generaste en el espacio)
 GOOGLE_CHAT_WEBHOOK = "https://chat.googleapis.com/v1/spaces/AAQAwN9u-kA/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=NWNC3za0Mbw2KWs8ctM5kqhaEMkGIzKgD8NL7dJeUos"
