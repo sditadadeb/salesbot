d0baca7bd09c0eadbf38bdf6e32d3c90fcef0515
sditadadeb <81593825+sditadadeb@users.noreply.github.com>
2025-08-23 15:15:07 -0300

    Update index.js

--- index.js
+++ index.js
@@ -3,10 +3,13 @@ const app = express();
 
 app.use(express.json());
 
+// Ruta principal que responde a Google Chat
 app.post('/', (req, res) => {
   const { message } = req.body;
 
-  console.log('Mensaje recibido:', message.text);
+  if (message && message.text) {
+    console.log('Mensaje recibido:', message.text);
+  }
 
   res.json({
     text: 'recibido'
