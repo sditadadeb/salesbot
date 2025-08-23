90e006f7a13510e963db66a003a2147bbc277c08
sditadadeb <81593825+sditadadeb@users.noreply.github.com>
2025-08-23 17:41:15 -0300

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,9 @@ const PORT = process.env.PORT || 3000;
 const SCOPES = [
   'https://www.googleapis.com/auth/chat.bot',
   'https://www.googleapis.com/auth/chat.messages',
+   'https://www.googleapis.com/auth/chat.spaces',
   'https://www.googleapis.com/auth/chat.spaces.readonly',
+    'https://www.googleapis.com/auth/chat.memberships'
   'https://www.googleapis.com/auth/chat.messages.readonly'
 ];
 
